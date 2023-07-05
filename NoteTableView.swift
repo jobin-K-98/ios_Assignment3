115fd67c906ec4f064d4692070e866d5091588da
jobin-K-98 <122489680+jobin-K-98@users.noreply.github.com>
2023-07-05 19:48:48 -0400

    Create NoteTableView.swift

---
+++ NoteTableView.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class NoteTableView: UITableViewController
+{
+    var noteList = [Note]()
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
