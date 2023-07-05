db765ef61cf5002fac87ad38e54e3e850decc086
jobin-K-98 <122489680+jobin-K-98@users.noreply.github.com>
2023-07-05 19:46:20 -0400

    Create NoteDetailVC.swift

---
+++ NoteDetailVC.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class NoteDetailVC: UIViewController {
+
+    @IBOutlet var titleTextField: UIView!
+    @IBOutlet var descTextView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func saveAction(_ sender: Any) {
+    }
+    
+}
+
